7e6559d47d09c4ad0cf905aea79d7b765707f32a
David Sanchez <dsanchezksab@gmail.com>
2018-03-09 06:24:46 -0500

    Kotlin auto data binding tested. Worked great.
    No need for findViewById or Butterknife approach.

--- app/src/main/java/com/budgetyourfuture/views/MainActivity.kt
+++ app/src/main/java/com/budgetyourfuture/views/MainActivity.kt
@@ -3,12 +3,14 @@ package com.budgetyourfuture.views
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.budgetyourfuture.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textview_helloworld.text = "Chao - Prueba"
         //ButterKnife.bind(this);
         //initComponents();
     }
